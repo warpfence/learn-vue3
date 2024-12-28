2cd6bf7ca169e2eaccf762f7faebfa3c3a0517be
Sehoon <ksstpgns1@gmail.com>
2024-12-28 15:27:21 +0900

    [:sparkles: Feat] Single-File Components

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title">Card title</h5>
+			<h5 class="card-title red">Card title</h5>
 			<p class="card-text">
 				Some quick example text to build on the card title and make up the bulk
 				of the card's content.
@@ -12,13 +12,33 @@
 </template>
 
 <script>
-console.log('AppCard Module');
+import { ref } from 'vue';
+
 export default {
 	setup() {
-		console.log('AppCard Setup');
-		return {};
+		const color = ref('red');
+		// color.value = 'yellow';
+		return {
+			color,
+		};
 	},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+.red {
+	color: v-bind(color) !important;
+}
+</style>
+
+<!-- <style module="classes">
+.red {
+	color: red !important;
+}
+</style> -->
+
+<!-- <style scoped>
+.red {
+	color: red !important;
+}
+</styl> -->
